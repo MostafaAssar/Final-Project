9cfb36d8922f7847d92cc6e6c3499cd3f55a8645
MostafaAssar <xdsasa3@gmail.com>
2023-12-07 05:04:00 +0200

    Login page

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import React from 'react';
 import SearchFilter from './components/Search/SearchFilter';
+import Login from './components/Login';
 function App() {
   return (
     <>
-    <SearchFilter />
+    {/* <SearchFilter /> */}
+    <Login />
   </>
   );
 }
