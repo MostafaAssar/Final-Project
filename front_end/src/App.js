e771d78a11a31876628214b7f6039e7945392a60
Mohamed Essam <msam01619@gmail.com>
2023-12-07 19:42:10 +0200

    list item solve prblem

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,20 +1,15 @@
 import './App.css';
 import React from 'react';
 import SearchFilter from './components/Search/SearchFilter';
-import SingleItem from './components/SingleItem';
 import Login from './components/Login';
 import ItemList from './components/ItemsList/ItemList';
 
 function App() {
   return (
     <>
-    {/* <ItemList/> */}
+    <ItemList/>
     {/* <SearchFilter /> */}
     {/* <Login /> */}
-<<<<<<< HEAD
-=======
-    {/* <SingleItem id = {"4"}/> */}
->>>>>>> e42d085d0a9ff414ec65fca48026c8944b39800d
   </>
   );
 }
