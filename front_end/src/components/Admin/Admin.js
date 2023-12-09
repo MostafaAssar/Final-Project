d15607db6efae0d7b938f0cf868d3eb9bc42283d
Mohamed Essam <msam01619@gmail.com>
2023-12-09 12:56:04 +0200

    admin html

--- front_end/src/components/Admin/Admin.js
+++ front_end/src/components/Admin/Admin.js
@@ -1,5 +1,20 @@
-import React from "react";
+
+import React, { useState } from 'react';
  const Admin =()=>{
-    
+     const [users, setusers]= useState([{name:'mo',id :"5"},{}]);
+     const deleteuser =(userId) => {
+        const updateusers = users.filter((user)=> user.id !== userId)
+        setusers(updateusers);
+     }
+    return(
+        <>
+         {users.map(user => (
+        <div key={user.id}>
+          <span>{user.name}</span>
+          <button onClick={() => deleteuser(user.id)}>Delete</button>
+        </div>
+      ))}
+        </>
+    )
  }
  export default Admin;
